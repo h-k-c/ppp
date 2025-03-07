57500736d2cb72c8fcdc823d275eb88e94677f07
openorange <2605245956@qq.com>
2025-03-07 15:49:08 +0800

    Initial Commit

---
+++ pwdpass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pwdpass
+//
+//  Created by 胡开成 on 2025/3/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pwdpass/pwdpassApp.swift
@@ -0,0 +1,17 @@
+//
+//  pwdpassApp.swift
+//  pwdpass
+//
+//  Created by 胡开成 on 2025/3/7.
+//
+
+import SwiftUI
+
+@main
+struct pwdpassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
